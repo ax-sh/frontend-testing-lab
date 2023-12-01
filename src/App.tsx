72f7671cc1b81ca1efc0c7d96385acb565474798
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-12-01 23:09:34 +0545

    feat: wip

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import { useRoutes } from "react-router-dom";
 import { Suspense } from "react";
 
 function App() {
-  console.log(routes);
-
   return <Suspense fallback={<p>Loading...</p>}>{useRoutes(routes)}</Suspense>;
 }
 
