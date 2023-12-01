903476ea8704c1bc6b077d61f4e53e5f7c391e5a
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-12-01 22:57:27 +0545

    feat: add component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
+import { Main } from "./components";
 import Layout from "./layouts";
 
 function App() {
   return (
     <Layout>
-      <h1 className="text-red-300 text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Main />
     </Layout>
   );
 }
---
+++ src/components/index.tsx
@@ -0,0 +1,5 @@
+export function Main() {
+  return (
+    <h1 className="text-red-300 text-3xl font-bold underline">Hello world!</h1>
+  );
+}
