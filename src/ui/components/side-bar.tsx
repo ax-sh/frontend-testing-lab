854bcceaf728e33dd6a3faac95420518e3115865
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-12-02 14:16:57 +0545

    refactor: style css

--- src/ui/components/side-bar.tsx
+++ src/ui/components/side-bar.tsx
@@ -4,7 +4,12 @@ export default function SideBar() {
   return (
     <aside className={"prose lg:prose-xl"}>
       <h6>Private</h6>
-      <FaBeer />
+      <ul>
+        <li>
+          {" "}
+          <FaBeer />
+        </li>
+      </ul>
     </aside>
   );
 }
