4cbb9f8990bcd2d17ff0a0b871b27316abd0547c
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-05-10 17:24:04 +0545

    feat: add component generation with tests and stories templates

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # frontend-testing-lab
 
-
 [//]: # (hygen generator with-prompt mygen)
 [//]: # (hygen mygen with-prompt)
 [//]: # (need to change module.exports to default export)
--- plopfile.ts
+++ plopfile.ts
@@ -6,11 +6,30 @@ export default function (plop: NodePlopAPI) {
   const plopStorybookAction: ActionType = {
     type: "add",
     path: `${wd}/src/ui/{{kebabCase targetDestinationFileName}}/{{kebabCase targetDestinationFileName}}.stories.tsx`,
-    skipIfExists: false,
+    skipIfExists: true,
     // templateFile: "templates/component.stories.tsx.ejs",
     templateFile: "templates/component.stories.tsx.ejs",
   };
-  const actions: ActionType[] = [plopStorybookAction];
+
+  const plopComponentTestAction: ActionType = {
+    type: "add",
+    path: `${wd}/src/ui/{{kebabCase targetDestinationFileName}}/{{kebabCase targetDestinationFileName}}.test.tsx`,
+    skipIfExists: true,
+    // templateFile: "templates/component.stories.tsx.ejs",
+    templateFile: "templates/component.test.tsx.ejs",
+  };
+  const plopComponentAction: ActionType = {
+    type: "add",
+    path: `${wd}/src/ui/{{kebabCase targetDestinationFileName}}/{{kebabCase targetDestinationFileName}}.tsx`,
+    skipIfExists: true,
+    // templateFile: "templates/component.stories.tsx.ejs",
+    templateFile: "templates/component.tsx.ejs",
+  };
+  const actions: ActionType[] = [
+    plopStorybookAction,
+    plopComponentTestAction,
+    plopComponentAction,
+  ];
   const config: Partial<PlopGeneratorConfig> = {
     description: "Generate a new React component with TypeScript",
     prompts: [
