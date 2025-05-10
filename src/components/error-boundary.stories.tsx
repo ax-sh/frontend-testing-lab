ca922b9e72fcfb4d3a005d41dcc99172e0bdd233
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-05-10 14:41:24 +0545

    feat(story): add background color control to args

--- src/components/error-boundary.stories.tsx
+++ src/components/error-boundary.stories.tsx
@@ -7,19 +7,16 @@ const meta = {
   component: ErrorBoundaryImpl,
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   args: { onClick: fn() },
+  //👇 Creates specific argTypes
+  argTypes: {
+    backgroundColor: { control: "color" },
+  },
 } satisfies Meta<typeof ErrorBoundaryImpl>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Idle: Story = {
-  args: {
-    onClick: fn(),
-    children: "Button",
-  },
-};
-
 export const ErrorActive: Story = {
   args: {
     onClick: fn(() => {
@@ -32,10 +29,9 @@ export const ErrorActive: Story = {
 
 export const ErrorOnClick: Story = {
   args: {
+    // ...ErrorActive.args,
     onClick: fn(() => {
       console.log("onClick ErrorOnClick");
     }),
-
-    children: "Error Button",
   },
 };
