d1d1f9697e90f39f8170318c47ccf1d314243f88
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-05-10 15:37:02 +0545

    chore: update Storybook dependencies and configuration

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,11 +4,9 @@ const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
 
   addons: [
-    "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@chromatic-com/storybook",
-    "@storybook/experimental-addon-test",
-    "@storybook/addon-mdx-gfm",
+    "@storybook/addon-vitest",
   ],
 
   framework: {
