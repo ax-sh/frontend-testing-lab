823e4d0905dc9b2bf41ad70cf769dc15a7d42011
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-05-10 17:02:28 +0545

    feat: add plop generator for React components with TSX

--- plopfile.ts
+++ plopfile.ts
@@ -1,5 +1,31 @@
+import type { PlopGeneratorConfig } from "node-plop";
 import type { ActionType, NodePlopAPI } from "plop";
-const wd = "apps/web";
+
+
+const wd = ".";
 export default function (plop: NodePlopAPI) {
-  const actions: ActionType[] = [];
+  const plopStorybookAction: ActionType = {
+    type: "add",
+    path: `${wd}/src/ui/{{kebabCase targetDestinationFileName}}/{{kebabCase targetDestinationFileName}}.stories.tsx`,
+    skipIfExists: false,
+    // templateFile: "templates/component.stories.tsx.hbs",
+    templateFile: "templates/component.stories.tsx.hbs",
+  };
+  const actions: ActionType[] = [plopStorybookAction];
+  const config: Partial<PlopGeneratorConfig> = {
+    description: "Generate a new React component with TypeScript",
+    prompts: [
+      {
+        type: "input",
+        name: "targetDestinationFileName",
+
+        message: "component name please",
+      },
+    ],
+    actions: (data) => {
+      console.debug("Plop generated data =>", data);
+      return actions;
+    },
+  };
+  plop.setGenerator("component", config);
 }
